3e8b1ac3b32ad772697239a5369f93504196e629
manish <manishkumaryadavcse@gmail.com>
2023-03-24 11:23:01 +0530

    message

---
+++ app/src/main/java/com/example/recipe/models/Detail.kt
@@ -0,0 +1,3 @@
+package com.example.recipe.models
+
+class Detail : ArrayList<DetailItem>()
---
+++ app/src/main/java/com/example/recipe/models/DetailItem.kt
@@ -0,0 +1,6 @@
+package com.example.recipe.models
+
+data class DetailItem(
+    val name: String,
+    val steps: List<Step>
+)
